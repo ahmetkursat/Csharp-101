4bfc7ff4c136562995955e60ecac9a058581f14b
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-08-27 13:44:12 +0300

    try catch

---
+++ HataYönetimi/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HataYönetimi
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] students = new string[3] { "Ahmet", "Kürşad", "YILDIZ" };
+
+                students[3] = "hasan";
+             }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ hatasınıfıYazıyorum/RecordNotFoundException.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatasınıfıYazıyorum
+{
+    public class RecordNotFoundException:Exception
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Find();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+
+            HandleExpection(() => { });
+ 
+            Console.ReadLine();
+
+
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string>() { "Ahmet", "Kürşad", "YILDIZ" };
+
+            if (!students.Contains("Ahmett"))
+            {
+                throw new RecordNotFoundException("Record Not Found");
+            }
+            else
+            {
+                Console.WriteLine("Record Bulundu");
+            }
+        }
+
+        public RecordNotFoundException(string message):base(message)
+        {
+
+        }
+    }
+}
